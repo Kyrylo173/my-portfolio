e7a39c89470e2dbb46fb54f943518723ed648cf0
Kyrylo173 <fogygom@gmail.com>
2026-03-07 12:44:08 +0100

    .

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export default function Projects() {
     const [currentImageIndex, setCurrentImageIndex] = useState({});
